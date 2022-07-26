6b2b65f1ded41bbc70b4b025018942b3fb799a5d
NoSilence <nosilence@list.ru>
2022-07-26 23:31:46 +0300

    MessageEntity fulfilled

--- internal/dto/types/message_entity.go
+++ internal/dto/types/message_entity.go
@@ -1,4 +1,32 @@
 package types
 
+const (
+	MessageEntityTypeMention       = "mention"
+	MessageEntityTypeHashtag       = "hashtag"
+	MessageEntityTypeCashtag       = "cashtag"
+	MessageEntityTypeBotCommand    = "bot_command"
+	MessageEntityTypeUrl           = "url"
+	MessageEntityTypeEmail         = "email"
+	MessageEntityTypePhoneNumber   = "phone_number"
+	MessageEntityTypeBold          = "bold"
+	MessageEntityTypeItalic        = "italic"
+	MessageEntityTypeUnderline     = "underline"
+	MessageEntityTypeStrikethrough = "strikethrough"
+	MessageEntityTypeSpoiler       = "spoiler"
+	MessageEntityTypeCode          = "code"
+	MessageEntityTypePre           = "pre"
+	MessageEntityTypeTextLink      = "text_link"
+	MessageEntityTypeTextMention   = "text_mention"
+)
+
+// MessageEntity
+// This object represents one special entity in a text message. For example, hashtags, usernames, URLs, etc.
+// https://core.telegram.org/bots/api#messageentity
 type MessageEntity struct {
+	Type     string `json:"type"`
+	Offset   int    `json:"offset"`
+	Length   int    `json:"length"`
+	Url      string `json:"url"`
+	User     User   `json:"user"`
+	Language string `json:"language"`
 }
